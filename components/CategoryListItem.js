e2e06a6555f0bca08f2436a6e643c10705746253
minh tuan <minhtuan2001611@gail.com>
2022-03-01 20:49:17 +0700

    tim hieu , project dau tien , styleSheet

---
+++ components/CategoryListItem.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+    Image,
+    Text,
+    View,
+    StyleSheet,
+} from 'react-native';
+import SkiiImage from '../assets/ski.png';
+export default function CategoryListItem(props) {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Category List</Text>
+            <Image style={styles.categoryImage} source={SkiiImage}></Image>
+        </View>
+    )
+}
+const mBottom = 16;
+const styles = StyleSheet.create({
+    container: {
+        width: 355,
+        height: 170,
+        backgroundColor: "yellow",
+        padding: 16,
+        alignItems: "center",
+        justifyContent: "center",
+        shadowColor: 'black', // màu đổ bóng
+        shadowOpacity: 0.5, // độ mờ
+        shadowRadius: 5, // độ tỏa 
+        shadowOffset: {  // độ lệch đổ bóng , (xuống , qua phải)
+            width: 0, height: 0,
+        },
+        marginBottom: mBottom,
+    },
+    title: {
+        textTransform: 'uppercase',
+        fontWeight: 'bold',
+    },
+    categoryImage: {
+        width: 64,
+        height: 64,
+    }
+})
